f6c2923e95a17509c011d2c9c346ee0d99040c53
harshanand2021 <harsh250600@gmail.com>
2024-01-08 21:37:46 +0530

    Array methods in javascript

---
+++ arrayMethod.js
@@ -0,0 +1,19 @@
+let arr = ['India',110,102.45,true]
+console.log(arr)
+
+let arr1 = new Array(10,56.23,false,"Besant")
+console.log("arr1 = ",arr1)
+
+//for loop iteration in array
+for(let index=0;index<4;index++){
+    console.log("arr[index] = ",arr[index],"index = ",index)
+}
+
+//push() method
+let color = ['yellow','orange','red','green']
+color.push('grey')
+console.log(color)
+
+//pop() method
+color.pop()
+console.log(color)
