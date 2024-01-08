04b346be4db632ae25e027fb08f888ab51910755
harshanand2021 <harsh250600@gmail.com>
2024-01-08 21:49:50 +0530

    assignment 1 done

---
+++ assignment.js
@@ -0,0 +1,23 @@
+let ar1 = [156,178.12,'India',true]
+console.log(ar1)
+
+//for loop iteration
+for(let index=0;index<4;index++){
+    console.log(ar1[index],"index = ",index)
+}
+
+//push() method
+ar1.push('Welcome')
+console.log(ar1)
+
+//unshift() method
+ar1.unshift(108)
+console.log(ar1)
+
+//pop() method
+ar1.pop()
+console.log(ar1)
+
+//shift() method
+ar1.shift()
+console.log(ar1)
