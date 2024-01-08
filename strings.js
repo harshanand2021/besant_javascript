7dbfcef8b7ec01991726aff27ad9ca2813996ad9
harshanand2021 <harsh250600@gmail.com>
2024-01-08 07:39:18 +0530

    Strings methods

---
+++ strings.js
@@ -0,0 +1,23 @@
+let x = 'apple'
+console.log(x.indexOf('e'))
+
+//lastIndexOf() concept
+console.log(x.lastIndexOf('l'))
+
+//match()
+let txt = 'mango yummy is so yummy'
+let pattern1 = /yummy/
+console.log(pattern1)
+
+let txt2 = 'I like programming language'
+console.log(txt2.slice(3,14))
+
+//substring()
+console.log(txt2.substring(5,20))
+
+//trim()
+txt3 = " Bhupendra Jogi "
+console.log(txt3.length)
+tr1 = txt3.trim()
+console.log(tr1)
+console.log(tr1.length)
